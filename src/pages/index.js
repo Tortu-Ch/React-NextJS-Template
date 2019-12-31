5cf0e971babdeaef5a391f8d0417d29e40a027d5
Godwin peter .O <me@godwin.dev>
2019-12-31 16:58:09 +0100

    Sample files

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Default = () => (
+  <div>
+    <div>
+Start
+      <Link href="/api"><a href="/">Next.js!</a></Link>
+    </div>
+  </div>
+);
+
+export default Default;
